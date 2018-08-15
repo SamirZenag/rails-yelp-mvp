f4029d0a94835f9ee2cbb50641eb1ed03de12e1f
Samir Zenag <samzenag@gmail.com>
2018-08-15 14:13:14 +0200

    yelp set up is done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      'London',
+    phone_number:  '+44 20 7700 3055',
+    category:      'chinese'
+  },
+  {
+    name:         'OLA',
+    address:      'Barcelona',
+    phone_number:  '+34 934 63 82 95',
+    category:      'french'
+  },
+  {
+    name:         'Arthur',
+    address:      'Paris',
+    phone_number:  '+33 1 46 23 45 54',
+    category:      'italian'
+  },
+  {
+    name:         'La gaufre',
+    address:      'Brussels',
+    phone_number:  '+32',
+    category:      'belgian'
+  },
+  {
+    name:         'Red District',
+    address:      'Amsterdam',
+    phone_number:  '+31 20 320 6100',
+    category:      'japanese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
