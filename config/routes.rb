b7ef9c5269dedb31d77b30e0507dc824ba265405
Samir Zenag <samzenag@gmail.com>
2018-08-15 18:48:18 +0200

    style added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :restaurants do
     resources :reviews, only: [:create, :new]
   end
+  root to: 'restaurants#index'
+
   namespace :admin do
     resources :restaurants, only: [:index]
   end
